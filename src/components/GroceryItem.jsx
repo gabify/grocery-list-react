7aca07c0e7659f00bdcf42c806d95a0eac118642
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2026-05-01 16:14:12 +0800

    Added category input upon creating new item

--- src/components/GroceryItem.jsx
+++ src/components/GroceryItem.jsx
@@ -3,7 +3,10 @@ const GroceryItem = ({ item, onDelete, onToggle }) => {
         <li className="flex items-center justify-between bg-gray-50 shadow-xs px-3 py-3 rounded-lg">
             <div className="flex items-center gap-2">
                 <input type="checkbox" checked={item.isBought} onChange={onToggle} />
-                <span className={`text-gray-800 ${item.isBought ? 'line-through' : ''}`}>{item.name}</span>
+                <div>
+                    <p className={`text-gray-800 ${item.isBought ? 'line-through' : ''}`}>{item.name}</p>
+                    <p className="text-xs text-gray-500">({item.category})</p>
+                </div>
             </div>
 
             <button className="text-red-400 text-sm" onClick={onDelete}>
